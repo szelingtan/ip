e6a8ee0c43c17bcbb2702a7766f88dff1f53299a
szelingtan <138017285+szelingtan@users.noreply.github.com>
2025-01-21 23:12:40 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/Tringa.java
@@ -0,0 +1,7 @@
+public class Tringa {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm tringa");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
