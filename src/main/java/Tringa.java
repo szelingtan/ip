074a50e54354eaa20c8f79b66fad8506e954bd86
szelingtan <138017285+szelingtan@users.noreply.github.com>
2025-01-22 22:42:49 +0800

    Level-1: Echo

--- src/main/java/Tringa.java
+++ src/main/java/Tringa.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class Tringa {
     public static void main(String[] args) {
         System.out.println("Hello! I'm tringa");
         System.out.println("What can I do for you?");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println(input);
+            input = sc.nextLine();
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
